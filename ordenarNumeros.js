380e3d9978038d8d2ca963f92482059f8d1606ac
Victor Martinez <vicmanuelg19@gmail.com>
2025-09-16 00:34:25 -0600

    feet:Se integran las funciones para ordenar los numeros y detectar si algún numero se repite. Se finaliza el ejercicio

--- ordenarNumeros.js
+++ ordenarNumeros.js
@@ -3,15 +3,62 @@ const entradaDeNumeros = document.querySelector('input');
 
 //llegamos a la etiqueta p con id "numerosDesordenados"
 const numerosIngresados = document.getElementById('numerosDesordenados')
-//Función para mostrar el valor escrito en input
 
-function mostrarNumero(){
-    let numeros = entradaDeNumeros.value;
-    numerosIngresados.innerText = numeros;
-    entradaDeNumeros.value = "";
+//llegamos a la etiqueta button por medio del DOM para manejar eventos
+const clickBoton = document.querySelector('button');
 
+// llegamos a la etiqueta p con id NumerosOrdenadosMenorAMayor
+const numerosMenorAMayor = document.getElementById('NumerosOrdenadosMenorAMayor');
+
+// llegamos a la etiqueta p con id NumerosOrdenadosMayorAMenor
+const numerosMayorAMenor = document.getElementById('NumerosOrdenadosMayorAMenor');
+
+//función con metodo burbuja para ordenar la lista de numeros
+function ordenarNumeros(arregloDenumeros) {
+    for (let i = 0; i < arregloDenumeros.length - 1; i++) {
+        for (let j = 0; j < arregloDenumeros.length - 1; j++) {
+            if (arregloDenumeros[j] > arregloDenumeros[j + 1]) {
+                let aux = arregloDenumeros[j];
+                arregloDenumeros[j] = arregloDenumeros[j + 1];
+                arregloDenumeros[j + 1] = aux;
+            }
+        }
+    }
+    return arregloDenumeros;
 }
 
-const clickBoton = document.querySelector('button');
+function numeroRepetido(arreglo) {
+    let confirmacionNumeroRepetido = false;
+    let numeroRepetido;
+    for (let i = 0; i < arreglo.length; i++) {
+        for (let j = 0; j < arreglo.length; j++)
+            //console.log(arreglo[i] + "--" + arreglo[j]);
+            if(arreglo[i] == arreglo[j] && i!=j){
+                confirmacionNumeroRepetido = true;
+                numeroRepetido = arreglo[i];
+            }
+    }
+
+    if(confirmacionNumeroRepetido == true){
+        alert(`El numero ${numeroRepetido} se repite dentro de los numeros ingresados`);
+    }
+}
+
+//Función para mostrar el valor escrito en input
+function mostrarNumero() {
+    let numeros = entradaDeNumeros.value;
+    numerosIngresados.innerText = numeros;
+    entradaDeNumeros.value = ""; // reseteamos el input
+    let arrayNumeros = numeros.split(",")//convertimos la cadena del input en un arreglo
+    ordenarNumeros(arrayNumeros);
+    const cadenaNumerosMenorAMayor = arrayNumeros.join(",");
+    numerosMenorAMayor.innerText = cadenaNumerosMenorAMayor; //Mostramos los numeros ordenamos por medio del DOM
+    const arrayNumerosInvert = arrayNumeros.reverse();
+    const cadenaNumerosMayorAMenor = arrayNumerosInvert.join(",");
+    numerosMayorAMenor.innerText = cadenaNumerosMayorAMenor;
+    numeroRepetido(arrayNumeros);
+}
+
+
 
 clickBoton.addEventListener('click', mostrarNumero);
