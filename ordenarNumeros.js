2ee9ec23add6ded64e256f4c2b3f052fd4a599b6
Victor Martinez <vicmanuelg19@gmail.com>
2025-09-15 22:45:36 -0600

    feet:Se termina el frontend de la pagina y por medio de js motramos los valores ingresados en el input de forma dinamica

--- ordenarNumeros.js
+++ ordenarNumeros.js
@@ -1,3 +1,17 @@
-function solicitudDeNumeros(params) {
-    
+//Obtenemos el valor del input desde el DOM
+const entradaDeNumeros = document.querySelector('input');
+
+//llegamos a la etiqueta p con id "numerosDesordenados"
+const numerosIngresados = document.getElementById('numerosDesordenados')
+//Función para mostrar el valor escrito en input
+
+function mostrarNumero(){
+    let numeros = entradaDeNumeros.value;
+    numerosIngresados.innerText = numeros;
+    entradaDeNumeros.value = "";
+
 }
+
+const clickBoton = document.querySelector('button');
+
+clickBoton.addEventListener('click', mostrarNumero);
